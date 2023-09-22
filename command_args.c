b958ac9ae46468c615fbfc6487daa745008878f8
Smart-Tech013 <festusbaylin@gmail.com>
2023-09-22 05:20:28 -0700

    Handle command lines with arguments

---
+++ command_args.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/*
+ * command_args - the function handles commalnn line arguments in the shell
+ * @input: user input/shell command 
+ */
+
+
+void command_args(const char *input)
+{
+    pid_t pid;
+    int status;
+
+    char *args[MAX_INPUT];
+    int i = 0;
+
+    char *token = strtok((char *)input, " ");
+    while (token != NULL)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    if (i == 0)
+    {
+        // No command provided
+        return;
+    }
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        if (waitpid(pid, &status, 0) == -1)
+        {
+            perror("wait terminated");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
