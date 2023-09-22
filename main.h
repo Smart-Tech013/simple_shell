9521ddf88c829392f6c1daee870e0eeec8513c94
Smart-Tech013 <festusbaylin@gmail.com>
2023-09-22 04:00:15 -0700

    All my header files

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_HEADER_H
+#define MAIN_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define MAX_INPUT 1024
+
+int process();
+int shellstructure();
+void commands(const char *input);
+
+#endif
